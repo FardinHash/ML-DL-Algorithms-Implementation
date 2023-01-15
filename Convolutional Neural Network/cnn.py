16e3eb5528009796eacf6b370b6ae1e90f91b227
Fardin <60337534+FardinHash@users.noreply.github.com>
2023-01-15 22:40:58 +0600

    cnn

---
+++ Convolutional Neural Network/cnn.py
@@ -0,0 +1,28 @@
+#import libraries
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+# Load the data
+(X_train, y_train), (X_test, y_test) = keras.datasets.cifar10.load_data()
+
+# Normalization of the data
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+
+# modle building
+model = keras.Sequential()
+model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(keras.layers.MaxPooling2D((2, 2)))
+model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(keras.layers.MaxPooling2D((2, 2)))
+model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(64, activation='relu'))
+model.add(keras.layers.Dense(10, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
